f3956499df14157ebcbd8a8999fc907bdf89b6f0
AdamPype <adam.pype@student.howest.be>
2018-11-05 14:28:18 +0100

    Init

---
+++ Assets/GoToLocationScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToLocationScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //get mouse position
+        Vector3 mousePos = Input.mousePosition;
+        Camera.main.ScreenPointToRay(mousePos);
+	}
+}
